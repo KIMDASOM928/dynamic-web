9bcc8bf6fe9c62e7f411fa972144e256cc9cd5f0
KIMDASOM928 <60718679+KIMDASOM928@users.noreply.github.com>
2020-03-25 17:54:21 -0400

    Add files via upload

---
+++ week7/script/fillRect.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#fd5c63";
+ctx.fillRect(0, 50, 250, 125);
---
+++ week7/script/main.js
@@ -0,0 +1,21 @@
+function draw() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+    } else {
+        const para = document.querySelector(".unsupported");
+        para.textContent = `Your browser does not support HTML5 Canvas`;
+    }
+
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    ctx.fillStyle = "#a51890";
+    ctx.fillRect(centerX - 140, centerY - 90, 300, 200);
+    ctx.clearRect(centerX - 130, centerY - 80, 280, 180);
+    ctx.strokeRect(centerX - 150, centerY - 100, 315, 215);
+
+}
+draw();
